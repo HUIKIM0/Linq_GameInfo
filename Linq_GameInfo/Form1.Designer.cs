b1640ca57bf32d586f0c6da8a616b5c3ecd7ec4b
HUIKIM0 <fehs123@naver.com>
2021-02-18 21:49:47 +0900

    update

--- Linq_GameInfo/Form1.Designer.cs
+++ Linq_GameInfo/Form1.Designer.cs
@@ -29,13 +29,145 @@ namespace Linq_GameInfo
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgGameInfo = new System.Windows.Forms.DataGridView();
+            this.btnLevel = new System.Windows.Forms.Button();
+            this.btnName = new System.Windows.Forms.Button();
+            this.btnAttribute = new System.Windows.Forms.Button();
+            this.cboxAttribute = new System.Windows.Forms.ComboBox();
+            this.numLevelMin = new System.Windows.Forms.NumericUpDown();
+            this.numLevelMax = new System.Windows.Forms.NumericUpDown();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgGameInfo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numLevelMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numLevelMax)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgGameInfo
+            // 
+            this.dgGameInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgGameInfo.Location = new System.Drawing.Point(13, 13);
+            this.dgGameInfo.Name = "dgGameInfo";
+            this.dgGameInfo.RowHeadersWidth = 51;
+            this.dgGameInfo.RowTemplate.Height = 27;
+            this.dgGameInfo.Size = new System.Drawing.Size(453, 517);
+            this.dgGameInfo.TabIndex = 0;
+            // 
+            // btnLevel
+            // 
+            this.btnLevel.Location = new System.Drawing.Point(485, 13);
+            this.btnLevel.Name = "btnLevel";
+            this.btnLevel.Size = new System.Drawing.Size(213, 49);
+            this.btnLevel.TabIndex = 1;
+            this.btnLevel.Text = "레벨 순으로";
+            this.btnLevel.UseVisualStyleBackColor = true;
+            // 
+            // btnName
+            // 
+            this.btnName.Location = new System.Drawing.Point(485, 78);
+            this.btnName.Name = "btnName";
+            this.btnName.Size = new System.Drawing.Size(213, 49);
+            this.btnName.TabIndex = 2;
+            this.btnName.Text = "이름 순으로";
+            this.btnName.UseVisualStyleBackColor = true;
+            // 
+            // btnAttribute
+            // 
+            this.btnAttribute.Location = new System.Drawing.Point(485, 142);
+            this.btnAttribute.Name = "btnAttribute";
+            this.btnAttribute.Size = new System.Drawing.Size(213, 49);
+            this.btnAttribute.TabIndex = 3;
+            this.btnAttribute.Text = "속성별로";
+            this.btnAttribute.UseVisualStyleBackColor = true;
+            // 
+            // cboxAttribute
+            // 
+            this.cboxAttribute.FormattingEnabled = true;
+            this.cboxAttribute.Location = new System.Drawing.Point(485, 242);
+            this.cboxAttribute.Name = "cboxAttribute";
+            this.cboxAttribute.Size = new System.Drawing.Size(213, 23);
+            this.cboxAttribute.TabIndex = 4;
+            // 
+            // numLevelMin
+            // 
+            this.numLevelMin.Location = new System.Drawing.Point(485, 289);
+            this.numLevelMin.Name = "numLevelMin";
+            this.numLevelMin.Size = new System.Drawing.Size(85, 25);
+            this.numLevelMin.TabIndex = 5;
+            // 
+            // numLevelMax
+            // 
+            this.numLevelMax.Location = new System.Drawing.Point(613, 289);
+            this.numLevelMax.Name = "numLevelMax";
+            this.numLevelMax.Size = new System.Drawing.Size(85, 25);
+            this.numLevelMax.TabIndex = 6;
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(485, 363);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(213, 49);
+            this.btnFilter.TabIndex = 7;
+            this.btnFilter.Text = "Filter";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(485, 428);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(213, 49);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(584, 294);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 15);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "~";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(718, 542);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.numLevelMax);
+            this.Controls.Add(this.numLevelMin);
+            this.Controls.Add(this.cboxAttribute);
+            this.Controls.Add(this.btnAttribute);
+            this.Controls.Add(this.btnName);
+            this.Controls.Add(this.btnLevel);
+            this.Controls.Add(this.dgGameInfo);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgGameInfo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numLevelMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numLevelMax)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgGameInfo;
+        private System.Windows.Forms.Button btnLevel;
+        private System.Windows.Forms.Button btnName;
+        private System.Windows.Forms.Button btnAttribute;
+        private System.Windows.Forms.ComboBox cboxAttribute;
+        private System.Windows.Forms.NumericUpDown numLevelMin;
+        private System.Windows.Forms.NumericUpDown numLevelMax;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Linq_GameInfo/Form1.cs
+++ Linq_GameInfo/Form1.cs
@@ -12,9 +12,99 @@ namespace Linq_GameInfo
 {
     public partial class Form1 : Form
     {
+        const string sLEVEL = "레벨";
+        const string sNAME = "이름";
+        const string sATTRIBUTE = "속성";
+
+        DataTable dt;
+
+        enum EnumName
+        {
+            슬라임,
+            가고일,
+            드래곤,
+            사신기사,
+            고블린,
+            언데드,
+            서큐버스,
+            골렘,
+            웜,
+            바실리스크,
+        }
+
+        enum EnumAttribute
+        {
+            불,
+            물,
+            바람,
+            번개,
+            어둠,
+            빛,
+            땅,
+        }
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTableCreate();
+            DataEnemy();
+        }
+
+        /* ★ 테이블 틀(레벨/이름/속성) 생성하기 ★ */
+        private void DataTableCreate()
+        {
+            dt = new DataTable("Enemy Info");
+
+            // 데이터 테이블 칼럼 생성
+            DataColumn colLevel = new DataColumn(sLEVEL, typeof(int));
+            DataColumn colName = new DataColumn(sNAME, typeof(string));
+            DataColumn colAttribute = new DataColumn(sATTRIBUTE, typeof(string));
+
+
+            dt.Columns.Add(colLevel);
+            dt.Columns.Add(colName);
+            dt.Columns.Add(colAttribute);
+
+        }
+
+
+        /* 테이블에 데이터 값 넣기 */
+        private void DataEnemy()
+        {
+            Random rd = new Random();
+
+            foreach (EnumName eName in Enum.GetValues(typeof(EnumName)))
+            {
+                DataRow dr = dt.NewRow();    //데이터 테이블에 새로운 Row 생성
+
+                dr[sLEVEL] = rd.Next(1, 11);  // 레벨에 들어가는 값은 1~ 10 중에서 랜덤
+                dr[sNAME] = eName.ToString();  // foreach문 돌려서 나오는 이름
+
+                int iEnumAttribute = Enum.GetValues(typeof(EnumAttribute)).Length;  //enum EnumAttribute 길이(개수) 뽑아옴
+                int iAttributeRandom = rd.Next(iEnumAttribute);  // enum 범위 내에서 랜덤
+                dr[sATTRIBUTE] = ((EnumAttribute)iAttributeRandom);   // 나온 랜덤숫자 다시 enum으로 반환
+
+
+                dt.Rows.Add(dr);
+            }
+
+            // DataGridView에 DataTable값들 넣어서 보여주기
+            dgGameInfo.DataSource = dt;
+        }
+
     }
+
+    /* 레벨/이름/속성 별로 정렬하기 */
+
+    /* 콤보박스 속성별로 정보보기  */
+
+    /* 필터 기능 */
+
+    /* 초기화 하고 처음화면 보여주기 */
+
 }
